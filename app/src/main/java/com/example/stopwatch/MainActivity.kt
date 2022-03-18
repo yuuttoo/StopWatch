ba824ca9f5aeada25624a282d1f6fddc8ac0968f
Sio <sio@SiodeMacBook-Air.local>
2022-03-18 16:19:15 +0800

    first commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.stopwatch
+
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.stopwatch.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+             startOrStop()
+        }
+
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
+
+        serviceIntent = Intent(applicationContext,StopWatchService::class.java)
+        registerReceiver(updateTime, IntentFilter(StopWatchService.UPDATED_TIME))
+
+    }
+    private fun startOrStop(){
+        if(isStarted)
+            stop()
+        else
+            start()
+    }
+
+    private fun start(){
+        serviceIntent.putExtra(StopWatchService.CURRENT_TIME, time)//pass data through intent
+        startService(serviceIntent)
+        binding.btnStart.text = "Stop"
+        isStarted = true
+    }
+
+    private fun stop(){
+        stopService(serviceIntent)
+        binding.btnStart.text = "Start"
+        isStarted = false
+    }
+
+    private fun reset(){
+        stop()
+        time = 0.0
+        binding.tvTime.text = getFormattedTime(time)
+    }
+
+    private val updateTime : BroadcastReceiver = object : BroadcastReceiver(){
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(StopWatchService.CURRENT_TIME, 0.0)
+            binding.tvTime.text = getFormattedTime(time)
+        }
+
+    }
+
+    //轉時間格式
+    private fun getFormattedTime(time:Double):String{
+        val timeInt = time.roundToInt()
+        val hours = timeInt % 86400 / 3600
+        val minutes = timeInt % 86400 % 3600 / 60
+        val seconds = timeInt % 86400 % 3600 % 60
+
+        return String.format("%02d:%02d:%02d",hours,minutes,seconds)
+    }
+
+
+}
